2579aeb41e8fd1b149e11836c2f31b8d24151c55
Leonardo Daitx <leonnardomelo3@gmail.com>
2023-11-02 21:35:10 -0300

    Front concept

--- view/src/components/Items.tsx
+++ view/src/components/Items.tsx
@@ -1,8 +1,32 @@
 import axios from "axios"
+import style from "./styles/items.module.css"
 
 const Items = () => {
   return (
-    <div>Items</div>
+    <>
+      <div className="entries">
+        <div className={ style.entry }>
+          <h2 className={ style.title }>Lancheria</h2>
+          <p style={{ marginBottom: '36px', }} className={ style.date }>12 de fevereiro de 2023</p>
+
+          <p style={{ marginBottom: '-8px' }} className={ style.beforeAndAfter }>Antes: R$ 1200</p>
+          <h2 style={{ marginBottom: '20px', color: '#A93333' }} className={ style.money }>R$ -112</h2>
+          <p className={ style.beforeAndAfter }>Depois: R$ 1088</p>
+
+          <div className={ style.category }>
+            <div>
+              <p>Gasto</p>
+            </div>
+            <div>
+              <p>Comida</p>
+            </div>
+            <div>
+              <p>Lazer</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
